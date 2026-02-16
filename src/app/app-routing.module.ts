8178ffda8afc7cf93bcb53211841c821eb8dba3d
CamilaAlcantara <camilamariana19@gmail.com>
2026-02-16 04:00:45 -0300

    feat: implementando a navbar e ajuste alert no autor-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'autores' },
+  { 
+    path: 'autores', 
+    loadChildren: () => import('./components/autor/autor.module').then(m => m.AutorModule) 
+  },
+  { 
+    path: 'assuntos', 
+    loadChildren: () => import('./components/assunto/assunto.module').then(m => m.AssuntoModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
